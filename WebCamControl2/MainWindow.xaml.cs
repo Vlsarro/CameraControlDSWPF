f8d56fee536eb0968c0d65c5181058f90eed451d
Vlsarro <drekmaro@gmail.com>
2015-11-29 00:52:49 +0300

    Added first version of code

---
+++ WebCamControl2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace WebCamControl2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebCam _webcam;
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Panel panel1 = new System.Windows.Forms.Panel();
+            formController.Child = panel1;
+            //formController.Child = new System.Windows.Forms.Button();
+            _webcam = new WebCam(panel1);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            //System.Windows.Forms.Integration.WindowsFormsHost host =
+              //  new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            
+            // Create the MaskedTextBox control.
+            //MaskedTextBox mtbDate = new MaskedTextBox("00/00/0000");
+
+            // Assign the MaskedTextBox control as the host control's child.
+            //host.Child = mtbDate;
+            
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            //this.grid1.Children.Add(host);
+        }
+
+        private void bntStart_Click(object sender, RoutedEventArgs e)
+        {
+
+            _webcam.Start_Preview();
+        }
+
+        private void bntStop_Click(object sender, RoutedEventArgs e)
+        {
+            _webcam.Stop_Preview();
+        }
+
+        private void bntStartCapture_Click(object sender, RoutedEventArgs e)
+        {
+            _webcam.Start_Capture(true);
+        }
+
+        private void bntStopCapture_Click(object sender, RoutedEventArgs e)
+        {
+            _webcam.Stop_Capture();
+        }
+
+    }
+}
---
+++ WebCamControl2/WebCam.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectX.Capture;
+using DShowNET;
+using System.Windows;
+
+namespace WebCamControl2
+{
+    class WebCam
+    {
+
+        Capture capture = null;
+        Filters filters = null;
+
+        int counter = 1;
+        bool startEnable = true;
+        private System.Windows.Forms.Panel _panel1;
+        //int deviceNumber = 0;
+
+
+        public WebCam(System.Windows.Forms.Panel panel1)
+        {
+            _panel1 = panel1;
+        }
+
+        public void Start_Preview()
+        {
+            filters = new Filters();
+
+            if (filters.VideoInputDevices != null)
+            {
+                try
+                {
+                    Console.WriteLine("****************\nPREVIEW FUNCTION\n*******************");
+                    Preview();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Maybe any other software is already using your device. \n\n Error Message: \n\n" + ex);
+                }
+            }
+            else
+            {
+                startEnable = false;
+                MessageBox.Show("No video device connected to you PC!");
+            }
+        }
+
+        public void Preview()
+        {
+            //TODO
+            //try
+            //{
+            
+            if (capture != null)
+            {
+                capture.Stop();
+                capture.PreviewWindow = null;
+            }
+                capture = new Capture(filters.VideoInputDevices[0], null);
+                //capture.VideoCompressor = filters.VideoCompressors[0];
+
+                capture.PreviewWindow = _panel1; //need to allocate wpf window for that
+
+                capture.Start();
+            //}
+            //catch { }
+        }
+        public void Stop_Preview()
+        {
+            Console.WriteLine("****************\nSTOP PREVIEW FUNCTION\n*******************");
+            capture.Stop();
+            capture.PreviewWindow = null;
+        }
+        public void Start_Capture(bool captureEnable)
+        {
+            Console.WriteLine("****************\nSTART CAPTURE FUNCTION\n*******************");
+
+            if (capture == null)
+            {
+                filters = new Filters();
+                if (filters.VideoInputDevices != null)
+                {
+                    try
+                    {
+                        capture = new Capture(filters.VideoInputDevices[0], null);
+                        capture.PreviewWindow = _panel1;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Maybe any other software is already using your device. \n\n Error Message: \n\n" + ex);
+                    }
+                }
+                else
+                {
+                    startEnable = false;
+                    MessageBox.Show("No video device connected to you PC!");
+                }
+            }
+            else
+            {
+                capture.Stop();
+                capture.VideoCompressor = filters.VideoCompressors[0];
+            }
+            
+
+            if (captureEnable == true)
+            {
+                counter++;
+                if (!capture.Cued) capture.Filename = counter + ".avi";
+                capture.Cue();
+            }
+            capture.Start();
+        }
+        public void Stop_Capture()
+        {
+            Console.WriteLine("****************\nSTOP CAPTURE FUNCTION\n*******************");
+            capture.Stop();
+            capture.PreviewWindow = null;
+        }
+    }
+}
